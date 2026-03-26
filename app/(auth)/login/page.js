d85c28bcc013dd227d22f3c43eb6499265144d1b
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-03-26 15:10:22 +0200

    feat: add Button component with primary and outline variants

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,9 +1,11 @@
 import Input from "@/app/components/ui/Input"
+import Button from "@/app/components/ui/Button"
 
 export default function LoginPage() {
   return (
     <div>
     <Input placeholder = "Email address"/>
+    <Button text="Log In" variant="primary" />
     </div>
   )
 }
---
+++ app/components/ui/Button.js
@@ -0,0 +1,10 @@
+export default function Button ({text, onClick, variant = 'primary'}){
+    const styles = {
+        primary: 'w-full py-3 bg-accent text-white rounded-lg font-medium'
+    }
+    return(
+        <button onClick={onClick} className={styles[variant]}>
+            {text}
+        </button>
+    )
+}
